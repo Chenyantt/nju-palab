3bf8728ded201a1ab804738e2fc6bd19ce575adf
tracer-ics2024 <tracer@njuics.org>
2024-12-20 17:01:23 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:23 up 2 days, 20:47,  1 user,  load average: 0.76, 0.44, 0.46

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -36,16 +36,15 @@ char *strcat(char *dst, const char *src)
 
 int strcmp(const char *s1, const char *s2)
 {
-  // assert((s1 != NULL) && (s2 != NULL));
-  // while (*s1 && (*s1 == *s2))
-  //   ++s1, ++s2;
-  // if (*s1 > *s2)
-  //   return 1;
-  // else if (*s1 < *s2)
-  //   return -1;
-  // else
-  //   return 0;
-  panic("Not implemented");
+  assert((s1 != NULL) && (s2 != NULL));
+  while (*s1 && (*s1 == *s2))
+    ++s1, ++s2;
+  if (*s1 > *s2)
+    return 1;
+  else if (*s1 < *s2)
+    return -1;
+  else
+    return 0;
   
 }
 
