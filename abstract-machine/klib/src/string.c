6678572d04bb916335aa1e022d4f891869ccbdfb
tracer-ics2024 <tracer@njuics.org>
2024-12-20 13:11:15 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:11:15 up 2 days, 16:57,  1 user,  load average: 1.22, 0.84, 0.52

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,85 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
+char *strcpy(char *dst, const char *src)
+{
+  assert((dst != NULL) && (src != NULL));
+  char *ret = dst;
+  while ((*dst++ = *src++))
+    ;
+  return ret;
+}
+
+char *strncpy(char *dst, const char *src, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
+char *strcat(char *dst, const char *src)
+{
+  assert((dst != NULL) && (src != NULL));
+  char *ret = dst;
+  for (; *dst; dst++)
+    ;
+  while ((*dst++ = *src++))
+    ;
+  return ret;
+}
+
+int strcmp(const char *s1, const char *s2)
+{
+  assert((s1 != NULL) && (s2 != NULL));
+  while (*s1 && (*s1 == *s2))
+    ++s1, ++s2;
+  if (*s1 > *s2)
+    return 1;
+  else if (*s1 < *s2)
+    return -1;
+  else
+    return 0;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
+void *memset(void *s, int c, size_t n)
+{
+  assert(s != NULL);
+  char *tmp = s;
+  while (n--)
+  {
+    *tmp++ = c;
+  }
+  return s;
+}
+
+void *memmove(void *dst, const void *src, size_t n)
+{
   panic("Not implemented");
 }
 
-int strcmp(const char *s1, const char *s2) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
-}
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  assert((s1 != NULL) && (s2 != NULL));
+  const unsigned char *su1, *su2;
+	int res = 0;
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
-}
-
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
-}
-
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+	for (su1 = s1, su2 = s2; 0 < n; su1++, su2++, n--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+	return res;
 }
 
 #endif
