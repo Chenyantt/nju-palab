22178e9ce561a867c0c58d77835ca616c326938f
Chenyantt <1154270143@qq.com>
2024-12-21 19:02:32 +0800

    finish iringbuf mtrace ftrace

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -6,7 +6,11 @@
 
 size_t strlen(const char *s)
 {
-  panic("Not implemented");
+  assert(s != NULL);
+  size_t len = 0;
+  while (*s++)
+    ++len;
+  return len;
 }
 
 char *strcpy(char *dst, const char *src)
@@ -20,7 +24,13 @@ char *strcpy(char *dst, const char *src)
 
 char *strncpy(char *dst, const char *src, size_t n)
 {
-  panic("Not implemented");
+  assert((dst != NULL) && src != (NULL));
+  char *ret = dst;
+  while (n-- && (*dst++ = *src++))
+    ;
+  while (n--)
+    *dst++ = '\0';
+  return ret;
 }
 
 char *strcat(char *dst, const char *src)
@@ -39,18 +49,17 @@ int strcmp(const char *s1, const char *s2)
   assert((s1 != NULL) && (s2 != NULL));
   while (*s1 && (*s1 == *s2))
     ++s1, ++s2;
-  if (*s1 > *s2)
-    return 1;
-  else if (*s1 < *s2)
-    return -1;
-  else
-    return 0;
-  
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
 {
-  panic("Not implemented");
+  assert((s1 != NULL) && (s2 != NULL));
+  if (!n)
+    return 0;
+  while (--n && *s1 && *s1 == *s2)
+    s1++, s2++;
+  return *s1 - *s2;
 }
 
 void *memset(void *s, int c, size_t n)
@@ -66,24 +75,64 @@ void *memset(void *s, int c, size_t n)
 
 void *memmove(void *dst, const void *src, size_t n)
 {
-  panic("Not implemented");
+  assert((dst != NULL) && (src != NULL));
+
+  if (src < dst)
+  {
+    char *pdst = (char *)dst + n - 1;
+    char *psrc = (char *)src + n - 1;
+    while (n--)
+    {
+      *pdst-- = *psrc--;
+    }
+  }
+  else
+  {
+    char *pdst = (char *)dst;
+    char *psrc = (char *)src;
+    while (n--)
+    {
+      *pdst++ = *psrc++;
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n)
 {
-  panic("Not implemented");
+  assert((out != NULL) && (in != NULL));
+
+  if (in < out)
+  {
+    char *pout = (char *)out + n - 1;
+    char *pin = (char *)in + n - 1;
+    while (n--)
+    {
+      *pout-- = *pin--;
+    }
+  }
+  else
+  {
+    char *pout = (char *)out;
+    char *pin = (char *)in;
+    while (n--)
+    {
+      *pout++ = *pin++;
+    }
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
   assert((s1 != NULL) && (s2 != NULL));
   const unsigned char *su1, *su2;
-	int res = 0;
+  int res = 0;
 
-	for (su1 = s1, su2 = s2; 0 < n; su1++, su2++, n--)
-		if ((res = *su1 - *su2) != 0)
-			break;
-	return res;
+  for (su1 = s1, su2 = s2; 0 < n; su1++, su2++, n--)
+    if ((res = *su1 - *su2) != 0)
+      break;
+  return res;
 }
 
 #endif
