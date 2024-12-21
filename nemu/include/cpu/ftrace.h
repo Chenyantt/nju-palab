d374ae11ff1d55904af88d032af3e6918caf2847
tracer-ics2024 <tracer@njuics.org>
2024-12-21 08:58:15 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:58:15 up 3 days,  6:50,  1 user,  load average: 1.72, 1.16, 0.74

---
+++ nemu/include/cpu/ftrace.h
@@ -0,0 +1,180 @@
+#ifndef __FTRACE_H__
+#define __FTRACE_H__
+
+
+
+#ifdef CONFIG_FTRACE
+#include <elf.h>
+#include <common.h>
+
+#define SYMSTR_SIZE 65536
+#define SYMTAB_SIZE 65536
+#define FTRACE_SIZE 65536
+
+typedef struct
+{
+    int type;
+    uint32_t pc;
+    char *name;
+    uint32_t dnpc;
+} FTraceLog;
+
+extern char symstr[SYMSTR_SIZE];
+extern Elf32_Sym sym_tab[SYMTAB_SIZE];
+extern uint32_t symtab_size;
+extern FTraceLog ftrace_log[FTRACE_SIZE];
+extern uint32_t ftracelog_size;
+
+static inline int init_ftrace(char *filename)
+{
+    Elf32_Ehdr ehdr;
+    FILE *fp = fopen(filename, "rb");
+    int ret;
+
+    if (fp == NULL)
+    {
+        // Log("Ftrace: Cannot open the elf file!");
+        return EXIT_FAILURE;
+    }
+    ret = fread((void *)(&ehdr), 1, sizeof(ehdr), fp);
+
+    uint16_t shentsize = ehdr.e_shentsize, shnum = ehdr.e_shnum;
+
+    if (fseek(fp, ehdr.e_shoff + ((uint32_t)shentsize) * ehdr.e_shstrndx, SEEK_SET) != 0)
+    {
+
+        // Log("Ftrace: Get the section header table failed!");
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+
+    Elf32_Shdr shdr;
+    ret = fread((void *)(&shdr), 1, sizeof(shdr), fp);
+
+    if (fseek(fp, shdr.sh_offset, SEEK_SET) != 0)
+    {
+        // Log("Ftrace: Get the shstr section failed!");
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+
+    char shstr[65536];
+    ret = fread((void *)shstr, 1, shdr.sh_size, fp);
+
+    if (fseek(fp, ehdr.e_shoff, SEEK_SET) != 0)
+    {
+        // Log("Ftrace: Get the section header table failed!");
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < shnum; ++i)
+    {
+        ret = fread((void *)(&shdr), 1, sizeof(shdr), fp);
+        if (strcmp(shstr + shdr.sh_name, ".strtab") == 0)
+        {
+            if (fseek(fp, shdr.sh_offset, SEEK_SET) != 0)
+            {
+                // Log("Ftrace: Get the symstr section failed!");
+                fclose(fp);
+                return EXIT_FAILURE;
+            }
+            ret = fread((void *)symstr, 1, shdr.sh_size, fp);
+            break;
+        }
+    }
+
+    if (fseek(fp, ehdr.e_shoff, SEEK_SET) != 0)
+    {
+        // Log("Ftrace: Get the section header table failed!");
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < shnum; ++i)
+    {
+        ret = fread((void *)(&shdr), 1, sizeof(shdr), fp);
+        if (strcmp(shstr + shdr.sh_name, ".symtab") == 0)
+        {
+            if (fseek(fp, shdr.sh_offset, SEEK_SET) != 0)
+            {
+                // Log("Ftrace: Get the symtab section failed!");
+                fclose(fp);
+                return EXIT_FAILURE;
+            }
+            uint32_t sym_num = shdr.sh_size / shdr.sh_entsize;
+            Elf32_Sym sym;
+            for (int j = 0; j < sym_num; ++j)
+            {
+                puts("1");
+                ret = fread((void *)(&sym), 1, sizeof(sym), fp);
+                printf("%d\n", sym.st_info);
+
+                if (ELF32_ST_TYPE(sym.st_info) == STT_FUNC)
+                {
+                    memcpy(&(sym_tab[symtab_size]), &sym, sizeof(sym));
+                    ++symtab_size;
+                }
+            }
+            break;
+        }
+    }
+    return ret;
+}
+
+static inline char *get_funcname(uint32_t pc)
+{
+    for (int i = 0; i < symtab_size; ++i)
+    {
+        if (sym_tab[i].st_value <= pc && sym_tab[i].st_value + sym_tab[i].st_size > pc)
+        {
+            return symstr + sym_tab[i].st_name;
+        }
+    }
+    return "???";
+}
+
+static inline int ftrace(uint32_t src, uint32_t dest, uint32_t pc, uint32_t dnpc)
+{
+    if (dest == 1)
+    {
+        ftrace_log[ftracelog_size].type = 0;
+        ftrace_log[ftracelog_size].pc = pc;
+        ftrace_log[ftracelog_size].dnpc = dnpc;
+        ftrace_log[ftracelog_size].name = get_funcname(dnpc);
+        ++ftracelog_size;
+        return 1;
+    }
+    else if (src == 1)
+    {
+        ftrace_log[ftracelog_size].type = 1;
+        ftrace_log[ftracelog_size].pc = pc;
+        ftrace_log[ftracelog_size].name = get_funcname(pc);
+        ++ftracelog_size;
+        return 2;
+    }
+    return 0;
+}
+
+static inline void display_ftrace()
+{
+    int depth = 0;
+    for (int i = 0; i < ftracelog_size; ++i)
+    {
+        printf("0x%08x: ", ftrace_log[i].pc);
+        for (int j = 0; j < depth; ++j)
+            putchar(' '), putchar(' ');
+        if (ftrace_log[i].type == 0)
+        {
+            printf("call [%s@0x%08x]\n", ftrace_log[i].name, ftrace_log[i].dnpc);
+        }
+        else
+        {
+            printf("ret  [%s]\n", ftrace_log[i].name);
+        }
+    }
+}
+
+
+#endif
+#endif
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -16,6 +16,15 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+#ifdef CONFIG_FTRACE
+#include <cpu/ftrace.h>
+char symstr[SYMSTR_SIZE];
+Elf32_Sym sym_tab[SYMTAB_SIZE];
+uint32_t symtab_size;
+FTraceLog ftrace_log[FTRACE_SIZE];
+uint32_t ftracelog_size;
+#endif
+
 void init_rand();
 void init_log(const char *log_file);
 void init_mem();
@@ -43,6 +52,7 @@ static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
+static char *elf_file = NULL;
 
 static long load_img() {
   if (img_file == NULL) {
@@ -72,16 +82,18 @@ static int parse_args(int argc, char *argv[]) {
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
+    {"elf"      , required_argument, NULL, 'e'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -89,6 +101,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=ELF_FILE       run Funtion Trace on ELF_FILE\n");
         printf("\n");
         exit(0);
     }
@@ -127,6 +140,8 @@ void init_monitor(int argc, char *argv[]) {
   init_sdb();
 
   IFDEF(CONFIG_ITRACE, init_disasm());
+
+  IFDEF(CONFIG_FTRACE, init_ftrace(elf_file));
 
   /* Display welcome message. */
   welcome();
