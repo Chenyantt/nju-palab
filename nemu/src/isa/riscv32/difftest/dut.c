9e1547c33e1f7198ae45671c12ac6a64fd0bb426
Chenyantt <1154270143@qq.com>
2024-12-22 00:21:15 +0800

    add difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -23,6 +23,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
   {
     if (gpr(idx) != ref_r->gpr[idx])
       return false;
+      
   }
   if (ref_r->pc != cpu.pc)
     return false;
