694a56bd35edaecdc9254ca9f7b7ad7b321226de
tracer-ics2024 <tracer@njuics.org>
2024-12-27 05:41:04 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:41:04 up 1 day,  3:32,  1 user,  load average: 0.27, 0.41, 0.34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,14 @@
 #include <common.h>
 #include "syscall.h"
 
+#define strace(); Log("[strace]:%s, a0:%d, a1:%d, a2:%d, a3:%d, ret:%d", syscall_name[a[0]], a[0], c->GPR1, c->GPR2, c->GPRx);
+
+static char* syscall_name[] = {"exit", "yield", "open", "read",
+                               "write", "kill", "getpid", "close",
+                               "lseek", "brk", "fstat", "time",
+                               "signal", "execve", "fork", "link",
+                               "unlink", "wait", "times", "gettimeofday"};
+
 static void do_yield(Context *c){
   yield();
   c->GPRx = 0;
@@ -13,11 +21,11 @@ static void do_exit(Context *c){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
   switch (a[0]) {
     case SYS_exit: do_exit(c); break;
     case SYS_yield: do_yield(c); break;
     case 2: 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  strace();
 }
