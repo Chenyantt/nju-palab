901ee2593054a244894ce70fe70580a41b98a7e0
tracer-ics2024 <tracer@njuics.org>
2024-12-26 23:55:00 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:55:00 up 21:46,  1 user,  load average: 0.08, 0.31, 0.38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,7 +40,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 void naive_uload(PCB *pcb, const char *filename)
 {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %u", entry);
+  Log("%d %u", entry, entry);
   Log("Jump to entry = %p", entry);
   ((void (*)())entry)();
 }
