3a41576acb8526381b95b2be54d67f21517a5292
tracer-ics2024 <tracer@njuics.org>
2024-12-27 05:42:54 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:42:54 up 1 day,  3:34,  1 user,  load average: 0.59, 0.47, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,15 +14,15 @@ static void do_yield(Context *c){
   c->GPRx = 0;
 }
 
-// static void do_exit(Context *c){
-//   halt(c->GPR2);
-// }
+static void do_exit(Context *c){
+  halt(c->GPR2);
+}
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   switch (a[0]) {
-    //case SYS_exit: do_exit(c); break;
+    case SYS_exit: strace();do_exit(c); break;
     case SYS_yield: do_yield(c); break;
     case 2: 
     default: panic("Unhandled syscall ID = %d", a[0]);
