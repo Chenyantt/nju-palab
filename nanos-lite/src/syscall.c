2a7bf1d2a33a58560792b0ec16e934ed46e9e148
tracer-ics2024 <tracer@njuics.org>
2024-12-27 09:59:04 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:59:04 up 1 day,  6:10,  1 user,  load average: 0.13, 0.29, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,32 +1,65 @@
 #include <common.h>
 #include "syscall.h"
 
-#define strace(); Log("[strace %s]: a0:%d, a1:%d, a2:%d, ret:%d", syscall_name[a[0]], a[1], c->GPR3, c->GPR4, c->GPRx);
+#define strace() \
+  ;              \
+  Log("[strace %s]: a0:%d, a1:%d, a2:%d, ret:%d", syscall_name[a[0]], a[1], c->GPR3, c->GPR4, c->GPRx);
 
-static char* syscall_name[] = {"exit", "yield", "open", "read",
+static char *syscall_name[] = {"exit", "yield", "open", "read",
                                "write", "kill", "getpid", "close",
                                "lseek", "brk", "fstat", "time",
                                "signal", "execve", "fork", "link",
                                "unlink", "wait", "times", "gettimeofday"};
 
-static void do_yield(Context *c){
+static void do_yield(Context *c)
+{
   yield();
   c->GPRx = 0;
 }
 
-static void do_exit(Context *c){
+static void do_exit(Context *c)
+{
   halt(c->GPR2);
 }
 
-void do_syscall(Context *c) {
+static void do_write(Context *c)
+{
+  uintptr_t fd = c->GPR2;
+  char *buf = (char *)c->GPR3;
+  uintptr_t len = c->GPR4;
+  if (fd == 0 || fd == 1)
+  {
+    for (uint32_t i = 0; i < len; ++i)
+    {
+      putch(buf[i]);
+    }
+    c->GPRx = len;
+  }
+  else
+  {
+    c->GPRx = -1;
+  }
+}
+
+void do_syscall(Context *c)
+{
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
-  switch (a[0]) {
-    case SYS_exit: strace();do_exit(c); break;
-    case SYS_yield: do_yield(c); break;
-    case 2: 
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  switch (a[0])
+  {
+  case SYS_exit:
+    strace();
+    do_exit(c);
+    break;
+  case SYS_yield:
+    do_yield(c);
+    break;
+  case SYS_write:
+    do_write(c);
+    break;
+  default:
+    panic("Unhandled syscall ID = %d", a[0]);
   }
   strace();
 }
