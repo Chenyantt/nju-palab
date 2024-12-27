e8475fe71afe100a2a91c25eaf2b27ca03cc2067
tracer-ics2024 <tracer@njuics.org>
2024-12-28 03:42:43 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:42:43 up 1 day, 15:28,  1 user,  load average: 0.61, 0.54, 0.48

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,7 +62,6 @@ size_t fs_read(int fd, void *buf, size_t len)
 {
   if (fd == 0 || fd == 1 || fd == 2)
     return 0;
-  Log("len: %d size: %d offset: %d\n",len,file_table[fd].size,file_table[fd].open_offset);
   if (file_table[fd].open_offset + len > file_table[fd].size)
     len = file_table[fd].size - file_table[fd].open_offset;
   size_t sz = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
@@ -94,6 +93,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
 
 size_t fs_lseek(int fd, size_t offset, int whence)
 {
+  Log("offset: %d whenceL %d\n",offset,whence);
   if (fd == 0 || fd == 1 || fd == 2)
     return 0;
   switch (whence)
