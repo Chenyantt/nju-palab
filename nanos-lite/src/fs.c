f5aaa1dd2547785f50562811504f8a29107eb899
tracer-ics2024 <tracer@njuics.org>
2024-12-28 05:58:15 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:58:15 up 1 day, 17:43,  1 user,  load average: 0.28, 0.30, 0.22

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -35,11 +35,20 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
   return 0;
 }
 
+static size_t serial_write(const void *buf, size_t offset, size_t len)
+{
+  for (size_t i = 0; i < len; ++i)
+  {
+    putch(((char *)buf)[i]);
+  }
+  return len;
+}
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -61,27 +70,26 @@ int fs_open(const char *pathname, int flags, int mode)
 size_t fs_read(int fd, void *buf, size_t len)
 {
   // Log("size: %d open_offset: %d len: %d off: %d\n",file_table[fd].size, file_table[fd].open_offset, len, file_table[fd].disk_offset + file_table[fd].open_offset);
-  if (fd == 0 || fd == 1 || fd == 2)
-    return 0;
-  if (file_table[fd].open_offset + len > file_table[fd].size)
-    len = file_table[fd].size - file_table[fd].open_offset;
-  size_t sz = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  assert(sz >= 0);
-  file_table[fd].open_offset += sz;
-  return sz;
+  if (file_table[fd].read)
+  {
+    return file_table[fd].read(buf, 0, len);
+  }
+  else
+  {
+    if (file_table[fd].open_offset + len > file_table[fd].size)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    size_t sz = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    assert(sz >= 0);
+    file_table[fd].open_offset += sz;
+    return sz;
+  }
 }
 
 size_t fs_write(int fd, const void *buf, size_t len)
 {
-  if (fd == 0)
-    return 0;
-  else if (fd == 1 || fd == 2)
+  if (file_table[fd].write)
   {
-    for (size_t i = 0; i < len; ++i)
-    {
-      putch(((char *)buf)[i]);
-    }
-    return len;
+    return file_table[fd].write(buf, 0, len);
   }
   else
   {
