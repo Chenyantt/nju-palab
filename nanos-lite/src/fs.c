cf291c93838926fb3c3843863cf4a9a61657705a
tracer-ics2024 <tracer@njuics.org>
2024-12-29 11:44:50 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:44:50 up 1 day, 22:16,  1 user,  load average: 0.73, 0.52, 0.53

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -75,7 +75,7 @@ size_t fs_read(int fd, void *buf, size_t len)
   }
   else
   {
-    printf("%d\n",fd);
+    printf("fd=%d,doff=%d,openoff=%d,len=%d\n",fd, file_table[fd].disk_offset, file_table[fd].open_offset, len);
     if (file_table[fd].open_offset + len > file_table[fd].size)
       len = file_table[fd].size - file_table[fd].open_offset;
     size_t sz = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
