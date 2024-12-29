4b8824507b10736fd8c8071c86e3dfe6e1ba82cb
tracer-ics2024 <tracer@njuics.org>
2024-12-29 09:52:08 +0800

    >  compile NEMU 1154270143@qq.com chenyantt Linux palab 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:52:08 up 1 day, 20:24,  1 user,  load average: 0.84, 0.56, 0.48

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,7 +24,6 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 
 size_t events_read(void *buf, size_t offset, size_t len)
 {
-  printf("1\n");
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE)
   {
