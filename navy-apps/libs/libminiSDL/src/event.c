adbe3b7247ea72d8e6cdd50e9e7b396ce2c1141d
Chenyantt <1154270143@qq.com>
2024-12-31 14:58:42 +0800

    in pa3-3

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,28 +2,43 @@
 #include <SDL.h>
 
 #define keyname(k) #k,
+#define BUFFER_SIZE 64
 
 static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+    "NONE",
+    _KEYS(keyname)};
 
-int SDL_PushEvent(SDL_Event *ev) {
+int SDL_PushEvent(SDL_Event *ev)
+{
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
+int SDL_PollEvent(SDL_Event *ev)
+{
   return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+int SDL_WaitEvent(SDL_Event *event)
+{
+  char buffer[BUFFER_SIZE];
+  int ret = NDL_PollEvent(buffer, BUFFER_SIZE);
+  if (ret == 0)
+    return 0;
+  if (*buffer == 'k')
+  {
+    event->type = (*(buffer + 1) == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
+    event->key = 
+  }SDL_Keys
+  return ret;
+  
 }
 
-int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
+{
   return 0;
 }
 
-uint8_t* SDL_GetKeyState(int *numkeys) {
+uint8_t *SDL_GetKeyState(int *numkeys)
+{
   return NULL;
 }
